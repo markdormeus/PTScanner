c767cbdd62279b9f13862d1d0e13e561918d4a17
markdormeus <serran.mark.ms@gmail.com>
2024-11-13 23:18:57 -0500

    Made it so that user can specify IP that they want to scan, added a welcome screen
    -Lists commands
    -Help menu
    -Quit button

--- main.rb
+++ main.rb
@@ -2,8 +2,30 @@ require 'socket'
 require 'timeout'
 require 'resolv'
 
-$stdout.sync = true  
-$mutex = Mutex.new   
+$stdout.sync = true
+$mutex = Mutex.new
+
+COMMON_PORTS = [21, 22, 23, 25, 53, 80, 110, 135, 139, 143, 443, 445, 993, 995, 1723, 3306, 3389, 5900, 8080]
+
+def print_banner
+  banner = <<-BANNER
+    ____  _____ ____   ____    _    _   _ _   _ _____ ____  
+   |  _ \\|_   _/ ___| / ___|  / \\  | \\ | | \\ | | ____|  _ \\ 
+   | |_) | | | \\___ \\| |     / _ \\ |  \\| |  \\| |  _| | |_) |
+   |  __/  | |  ___) | |___ / ___ \\| |\\  | |\\  | |___|  _ < 
+   |_|     |_| |____/ \\____/_/   \\_\\_| \\_|_| \\_|_____|_| \\_\\
+                                                            
+  BANNER
+  puts banner
+end
+
+def print_info
+  puts "       =[ PTSCANNER v1.0.0                              ]"
+  puts "+ -- --=[ Port scanning made easy                        ]"
+  puts "+ -- --=[ Scanning common ports                          ]"
+  puts "PTSCANNER tip: Use 'help' to see available commands"
+  puts
+end
 
 def scan_port(ip, port, timeout = 1)
   begin
@@ -13,10 +35,8 @@ def scan_port(ip, port, timeout = 1)
       return :open
     end
   rescue Errno::ECONNREFUSED
-    $mutex.synchronize { puts "Port #{port} is closed" }
     return :closed
   rescue Timeout::Error
-    $mutex.synchronize { puts "Port #{port} timed out" }
     return :filtered
   rescue SocketError => e
     $mutex.synchronize { puts "Error scanning port #{port}: #{e.message}" }
@@ -27,13 +47,13 @@ def scan_port(ip, port, timeout = 1)
   end
 end
 
-def scan_ports(ip, start_port, end_port, threads = 10)
-  puts "Scanning ports #{start_port} to #{end_port} on #{ip}"
+def scan_ports(ip, threads = 10)
+  puts "Scanning common ports on #{ip}"
   results = { open: 0, closed: 0, filtered: 0, error: 0 }
-  total_ports = end_port - start_port + 1
+  total_ports = COMMON_PORTS.length
   scanned_ports = 0
 
-  (start_port..end_port).each_slice(threads) do |ports|
+  COMMON_PORTS.each_slice(threads) do |ports|
     threads = ports.map do |port|
       Thread.new do
         result = scan_port(ip, port)
@@ -49,15 +69,42 @@ def scan_ports(ip, start_port, end_port, threads = 10)
   results.each { |status, count| puts "#{status.capitalize}: #{count}" }
 end
 
+def main_loop
+  loop do
+    print "ptscanner > "
+    input = gets.chomp.split
+    command = input.shift
 
-hostname = 'example.com'  
-start_port = 1
-end_port = 100  
-
-begin
-  ip = Resolv.getaddress(hostname)
-  puts "Resolved #{hostname} to #{ip}"
-  scan_ports(ip, start_port, end_port)
-rescue Resolv::ResolvError => e
-  puts "Could not resolve hostname: #{e.message}"
+    case command
+    when "exit", "quit"
+      puts "Exiting PTSCANNER..."
+      break
+    when "scan"
+      if input.length != 1
+        puts "Usage: scan <hostname/ip>"
+      else
+        hostname = input[0]
+        begin
+          ip = Resolv.getaddress(hostname)
+          puts "Resolved #{hostname} to #{ip}"
+          scan_ports(ip)
+        rescue Resolv::ResolvError => e
+          puts "Could not resolve hostname: #{e.message}"
+        end
+      end
+    when "help"
+      puts "Available commands:"
+      puts "  scan <hostname/ip> - Scan common ports on specified host"
+      puts "  help - Show this help message"
+      puts "  exit/quit - Exit PTSCANNER"
+    else
+      puts "Unknown command. Type 'help' for available commands."
+    end
+  end
 end
+
+# Main execution
+system("clear") || system("cls")  # Clear the screen
+print_banner
+print_info
+main_loop
